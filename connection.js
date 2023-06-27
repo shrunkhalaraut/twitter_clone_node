30f3968f942adc31ee788b44f197feea6e4c7337
shrunkhalaraut <shrunkhalaraut@gmail.com>
2023-06-27 23:01:38 +0530

    mongo error

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const { mongo } = require("mongoose");
 
-const mongoose = require(mongoose);
+const mongoose = require("mongoose");
 const url= "mongodb+srv://shrunkhalaraut:D9QoQLb9zGiVi7sJ@cluster0.dwabuzw.mongodb.net/blogdb"
 
 async function connectionMongoDB(){
