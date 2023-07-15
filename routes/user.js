58e8b2f018fa69fdd88793c0088608ea04ad83a7
shrunkhalaraut <shrunkhalaraut@gmail.com>
2023-07-16 00:46:28 +0530

    signup

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user')
 
-router.post('signup', userController.signupUser)
+router.post('/signup', userController.signupUser)
 
 router.post('/login', userController.login)
 
